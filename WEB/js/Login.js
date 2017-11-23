8bdda391f6bdd278917c5689519eac99d15cac65
gtobio11 <gtobio11@gmail.com>
2017-11-23 22:31:14 +0900

    Finished Login Fage (WEB)

---
+++ WEB/js/Login.js
@@ -0,0 +1,24 @@
+function doLogin(){
+    console.log(1);
+    $.ajax({
+        url : "/account/login"
+        type : "POST",
+        data : {
+            id : $('#LoginID').val(),
+            pw : $('#LoginPW').val()
+        },
+        success : function(data, status){
+            location.href = "./index.html";
+        },
+        error : function(xhr) {
+            alert("로그인에 실패했습니다.");
+            location.reload();
+        }
+    });
+}
+$("#LoginSubmit").on('click',function(){
+    doLogin();
+});
+$("#GoRegister").on('click',function(){
+    location.href="./Register.html";
+})
