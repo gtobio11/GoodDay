a08591a34d1a6e981c5a32ebd1e682d480da2a65
gtobio11 <gtobio11@gmail.com>
2017-11-27 23:07:46 +0900

    Finished Web Front

---
+++ WEB/js/index.js
@@ -0,0 +1,104 @@
+var date = new Date();
+
+var dateYear = date.getFullYear();
+var dateMonth = date.getMonth() + 1;
+var dateDate = date.getDate();
+
+$("#HWInputDeadline").append("<input type = 'date' id='HWDeadline' value='"+dateYear + "-" + dateMonth + "-" + dateDate + "' name='date'>");
+
+$("#HWFade").on("click",function(){
+    $("#HWUploadWindow").toggleClass("fade-out");
+    $("#HWUploadWindow").toggleClass("fade-in")
+})
+$("#HWFade-out").on("click",function(){
+    $("#HWUploadWindow").toggleClass("fade-out");
+})
+
+$("form#HWUploadForm").submit(function(event){
+    event.preventDefault();
+    
+    var formData = new FormData($(this)[0]);
+
+    $.ajax({
+        type : "POST",
+        url : "/homework",
+        data : formData,
+        contentType : false,
+        processData : false,
+        success : function(){
+            $("HWUploadWindow").toggleClass("fade-out");
+        },
+        error : function(){
+            alert("과제 업로드에 실패하였습니다.");
+        }
+    })
+})
+
+function getHWList(){
+    $.ajax({
+        url : "/homework",
+        type : "GET",
+        success : function(data){
+            var parsedData = JSON.parse(data).result;
+            parsedData.foreach(function(data){
+                makeHWList(data,$('#HWList'));
+            });
+        }
+    });
+}
+function makeHWList(data, target){
+    if(data.hurry>=4){
+        var newCard = $('<div/>',{
+            "class" : "HWListLoad " + "HWListHurry"
+        });
+    }
+    else if(data.hurry<=3){
+        var newCard = $('<div/>',{
+            "class" : "HWListLoad " + "HWListNotHurry"
+        });
+    }
+    newCard.append($('<div/>',{
+        "class" : "HWListTitle",
+        text : data.title
+    }));
+    newCard.append($('<div/>',{
+        "class" : "HWListContent",
+        text : data.content
+    }));
+    newCard.append($('<div/>',{
+        "class" : "HWListDeadline",
+        text : "마감일 " + data.deadline
+    }));
+    newCard.on('click',function(){
+        $("#HWPopUp").toggleClass("fade-out");
+        $("#HWPopUp").toggleClass("fade-in");
+        var HWBtnGroup = "<a href='/download/" + data.Filenum + "' class='HWDownloadBtn'>지금 할래~</a>";
+        if(data.hurry == 1){
+            var HWTitle = "<span id='HWPopUpDetailTitle'>아직은 여유로워~</span>";
+            var HWBtnGroup = "<a href='/download/" + data.Filenum + "' class='HWDownloadBtn2'>지금 할래~</a><button id='HWPopUpClose'>응 아니야~</button>";
+        }
+        else if(data.hurry == 2){
+            var HWTitle = "<span id='HWPopUpDetailTitle'>내일 해도 될거같아~</span>";
+            var HWBtnGroup = "<a href='/download/" + data.Filenum + "' class='HWDownloadBtn2'>지금 할래~</a><button id='HWPopUpClose'>응 아니야~</button>";
+        }
+        else if(data.hurry == 3){
+            var HWTitle = "<span id='HWPopUpDetailTitle'>오늘 하는게 좋을것 같아~</span>";
+            var HWBtnGroup = "<a href='/download/" + data.Filenum + "' class='HWDownloadBtn2'>지금 할래~</a><button id='HWPopUpClose'>응 아니야~</button>";
+        }
+        else if(data.hurry == 4){
+            var HWTitle = "<span id='HWPopUpDetailTitle'>오늘 무조건 해야되 !</span>";
+            var HWBtnGroup = "<a href='/download/" + data.Filenum + "' class='HWDownloadBtn'>지금 할래~</a>";
+        }
+        else if(data.hurry == 5){
+            var HWTitle = "<span id='HWPopUpDetailTitle'>창밖으로 떨어져 ~</span>"
+            var HWBtnGroup = "<a href='/download/" + data.Filenum + "' class='HWDownloadBtn'>지금 할래~</a>";
+        }
+        $("#HWPopUpTitle").append(HWTitle);
+        $("#HWPopUpAcc").append(HWBtnGroup);
+    })
+    $("#HWListTitle").append(newCard);
+}
+$("#HWPopUpClose").on("click",function(){
+    $("#HWPopUp").toggleClass("fade-out");
+    $("#HWPopUp").toggleClass("fade-in");
+});
